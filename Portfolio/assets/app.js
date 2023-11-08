c6e586254cac1f216a849f03c935764bff08995f
Tony <tonymichaud987@gmail.com>
2023-11-08 19:11:49 +0100

    second commit

--- Portfolio/assets/app.js
+++ Portfolio/assets/app.js
@@ -6,14 +6,14 @@ let showOverlay = document.querySelector('.overlay')
 
 let burgerBtn = document.getElementById('burger')
 let navShow = document.getElementById('navigator')
-revealModal.addEventListener('click', () => {
-        showModal.style.display = "block";
-        showOverlay.style.display = "block";
-});
-closeModal.addEventListener('click', () => {
-    showModal.style.display = "none"
-    showOverlay.style.display = "none";
-})
+// revealModal.addEventListener('click', () => {
+//         showModal.style.display = "block";
+//         showOverlay.style.display = "block";
+// });
+// closeModal.addEventListener('click', () => {
+//     showModal.style.display = "none"
+//     showOverlay.style.display = "none";
+// })
 
 burgerBtn.addEventListener('click', () => {
     navShow.classList.toggle('active')
